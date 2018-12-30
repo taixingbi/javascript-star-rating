970c9d6c7d62d6663dc7b3b1b455e7b3649d5b72
taixingbi <bitaihang@gmail.com>
2018-12-30 18:29:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function ratingStar(event){
     var checkValue = document.querySelectorAll("input");
     var checkStar = document.querySelectorAll("label");
+    
     var cnt = 0; //how many stars chosen
-
     for(var i=0; i<checkValue.length; i++){  //number of stars = 5
         if(checkValue[i]==event.target){     //number of stars chosen
             cnt = i+1;
